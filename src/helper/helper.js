a01c9bbb08ba5dbc2141fdf14e055313f5e5c9a8
mahdieh Nemati <104289694+mahdiehNm@users.noreply.github.com>
2024-04-24 22:20:41 +0430

    added custom hooks for products filter

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -2,4 +2,48 @@ const shortenText = (text) => {
   return text.split(" ").slice(0, 3).join("");
 };
 
-export { shortenText };
+const searchProducts = (products, search) => {
+  if (!search) return products; //اگر سرچی انجام نشده بود همه محصولات را نمایش بده
+  const searchedProducts = products.filter((p) =>
+    p.title.toLowerCase().includes(search)
+  );
+  return searchedProducts;
+};
+
+const filterProducts = (products, category) => {
+  if (!category) return products; //اگر دسته بندی ای انتخاب نشده بود همه محصولات را نمایش بده
+  const filteredProducts = products.filter((p) => p.category === category);
+  return filteredProducts;
+};
+
+const createQueryObject = (currentQuery, newQuery) => {
+  if (newQuery.category === "all") {
+    const { category, ...rest } = currentQuery;
+    return rest;
+  }
+
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+
+  return { ...currentQuery, ...newQuery };
+};
+
+//این فانکشن برای زمانی استفاده میشه که اگر فیلتر انجام داده باشید بر روی محصولات با یک ریلوود این فیلتر از بین نره
+const getInitialQuery = (searchParams) => {
+  const query = {};
+  const category = searchParams.get("category");
+  const search = searchParams.get("search");
+  if (category) query.category = category;
+  if (search) query.search = search;
+  return query;
+};
+
+export {
+  shortenText,
+  searchProducts,
+  filterProducts,
+  createQueryObject,
+  getInitialQuery,
+};
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,20 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
+import Loader from "../components/Loader";
+import { ImSearch } from "react-icons/im";
+import {
+  createQueryObject,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
+} from "../helper/helper";
+import { useSearchParams } from "react-router-dom";
+// import { FaListUL } from "react-icons/fa";
+// import { Link } from "react-router-dom";
 
 const ProductsPage = () => {
-  const products = useProducts();
+  const products = useProducts(); //use from globalData
+  const [search, setSearch] = useState("");
+  const [displayed, setDisplayed] = useState([]);
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitialQuery(searchParams)); //با ریلوود دیتاهای فیلتر شده از بین نروند
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
+  const searchHandler = () => {
+    // setQuery((query) => ({ ...query, search: search }));
+    setQuery((query) => createQueryObject(query, { search }));
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return; //یعنی اگه اون المان ال ای نبود این فانکشن اجرا نشود
+    // setQuery((query) => ({ ...query, category }));
+    setQuery((query) => createQueryObject(query, { category }));
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <p>Loading...</p>}
-        {products.map((p) => (
-          <Card key={p.id} data={p}/>
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search..."
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+          value={search}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>
+          <div>
+            {/* <FaListUL /> */}
+            <p>categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
